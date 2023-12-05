8d51ebf5e057441895383204319cf28a21b13d80
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2023-12-05 14:23:37 +0000

    Day 2

--- 2023/day2/main.go
+++ 2023/day2/main.go
@@ -4,7 +4,15 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
+
+var cubes = map[string]int{
+	"red":   12,
+	"green": 13,
+	"blue":  14,
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -44,7 +52,53 @@ func readInput(filename string) ([]string, error) {
 }
 
 func solvePart1(input []string) int {
-	return 0
+	sum := 0
+	for _, line := range input {
+		s := strings.Split(line, ":")
+
+		gameID, err := strconv.Atoi(strings.Split(s[0], " ")[1])
+		fmt.Println(gameID)
+		if err != nil {
+			fmt.Println("Error converting gameID:", err)
+			return 0
+		}
+		rounds := strings.Split(s[1], ";")
+
+		maxRed := 0
+		maxGreen := 0
+		maxBlue := 0
+
+		for _, round := range rounds {
+			cube := strings.Split(round, ",")
+			for _, c := range cube {
+				str := strings.Split(c, " ")
+				value, err := strconv.Atoi(str[1])
+				if err != nil {
+					fmt.Println("Error converting value:", err)
+					return 0
+				}
+				color := str[2]
+
+				switch color {
+				case "red":
+					if value > maxRed {
+						maxRed = value
+					}
+				case "green":
+					if value > maxGreen {
+						maxGreen = value
+					}
+				case "blue":
+					if value > maxBlue {
+						maxBlue = value
+					}
+				}
+			}
+		}
+
+		sum += (maxRed * maxGreen * maxBlue)
+	}
+	return sum
 }
 
 func solvePart2(input []string) int {
